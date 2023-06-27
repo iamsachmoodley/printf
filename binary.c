4aab59d30390d8eea860ff45dfb613413c4ef51d
iamsachmoodley <sachmoodley@gmail.com>
2023-06-27 11:56:03 -0700

    Update

---
+++ binary.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**PRINT BINARY**/
+/**
+ * print_binary - Prints the unsigned number
+ * @types: List of arguments
+ * @buffer: Buffer array used for print
+ * @flags:  Used to calculates the active flags
+ * @width: The width
+ * @precision: Precision specification that is required
+ * @size: Size specification that is required
+ * Return: Number of char printed
+ */
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648;
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
